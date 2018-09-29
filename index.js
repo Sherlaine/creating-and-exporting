fd95c0e2893a1ef4baa433a0c23fa66c7d6d8f9a
Sherlaine <sherlaine.lau@gmail.com>
2018-09-29 15:00:25 +0000

    index file with importing

---
+++ index.js
@@ -0,0 +1,10 @@
+var stores = require("./module-exercise");
+
+stores.storeNumbers(9);
+stores.storeNumbers(8);
+stores.storeNumbers(7);
+stores.storeNumbers(6);
+stores.storeNumbers(5);
+var resolvedList = stores.sortsNumbers()
+
+console.log(resolvedList)
